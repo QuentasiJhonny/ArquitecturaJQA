b8868de0cbfda86ad207c70f85e938f5526f5b7b
quentasijhonny <quentasijhonny@gmail.com>
2020-05-10 01:09:18 -0400

    09/05/2020

--- CompositeCursos/src/Main.java
+++ CompositeCursos/src/Main.java
@@ -11,41 +11,41 @@
 public class Main {
     public static void main(String [] args){
        //creacion de temas 
-       Temas tema1 = new Temas("Introduccion","Aprender lo basico","base1",124,72.0f);
-       Temas tema2 = new Temas("Introduccion2","Aprender lo basico","base2",378,14.0f);
-       Temas tema3 = new Temas("Introduccion3","Aprender lo basico","base3",24,54.0f);
-       Temas tema4 = new Temas("Introduccion4","Aprender lo basico","base4",12,74.0f);
-       Temas tema5 = new Temas("Introduccion5","Aprender lo basico","base5",72,84.0f);
-       Temas tema6 = new Temas("Introduccion6","Aprender lo basico","base6",48,94.0f);
-        Temas tema7 = new Temas("Introduccion7","Aprender lo basico","base7",12,124.0f);
+       Temas tema1 = new Temas("Introduccion a la programacion","Aprender lo basico","base1",124,72.0f);
+       Temas tema2 = new Temas("Introduccion a la programacion2","Aprender lo basico","base2",378,14.0f);
+       Temas tema3 = new Temas("Introducciona a la programacion 3","Aprender lo basico","base3",24,54.0f);
+       Temas tema4 = new Temas("Introduccion a la programacion 4","Aprender lo basico","base4",12,74.0f);
+       Temas tema5 = new Temas("Introduccion a la programacion 5","Aprender lo basico","base5",72,84.0f);
+       Temas tema6 = new Temas("Introduccion a la programacion 6","Aprender lo basico","base6",48,94.0f);
+        Temas tema7 = new Temas("Introduccion a la programacion7","Aprender lo basico","base7",12,124.0f);
        
        //creacion de cursos
        
-       Curso curso= new Curso("curso1");
+       Curso curso= new Curso("programacion 1");
        curso.addTemas(tema1);
        curso.addTemas(tema2);
        curso.addTemas(tema3);
-       Curso curso1= new Curso("curso2");
+       Curso curso1= new Curso("programacion 2");
        curso.addTemas(tema1);
        curso.addTemas(tema2);
        curso.addTemas(tema3);
        curso.addTemas(tema4);
        curso.addTemas(tema5);
        
-       Curso curso2= new Curso("curso3");
+       Curso curso2= new Curso("programacion 3");
        curso.addTemas(tema1);
        curso.addTemas(tema2);
        curso.addTemas(tema3);
        curso.addTemas(tema6);
        curso.addTemas(tema7);
        
-       Curso curso3= new Curso("curso4");
+       Curso curso3= new Curso("programacion 5");
        
        curso.addTemas(tema1);
        curso.addTemas(tema2);
        curso.addTemas(tema3);
         curso.addTemas(tema4);
-       Curso curso4= new Curso("curso5");
+       Curso curso4= new Curso("programacion web");
         curso.addTemas(tema1);
        curso.addTemas(tema2);
        curso.addTemas(tema3);
